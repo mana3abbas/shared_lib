a2a40dfe6b2e25e60867c5f5595e2b0466f7178d
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-05-19 13:01:13 +0300

    Update helm.groovy

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -1,8 +1,25 @@
 def call(){
-                sh 'cd HELM'  
-                sh 'pwd'
-                sh """
-                         echo "Running Helm"
-                         helm install vois${BUILD_NUMBER} ./HELM/onboard-task
-                   """   
+                 if (params.DEPLOY == 'dev')
+                                {
+                                 
+                                  sh """
+                                    echo "Running Helm"
+                                   helm install dev${BUILD_NUMBER} ./HELM/onboard-task --values ./HELM/onboard-task/dev.yaml
+                                    """
+                               }
+                          else if (params.DEPLOY == 'test')
+                          {
+                                  sh """
+                                    echo "Running Helm"
+                                   helm install test${BUILD_NUMBER} ./HELM/onboard-task --values ./HELM/onboard-task/test.yaml
+                                    """
+                               }
+                         else if (params.DEPLOY == 'prod')
+                                {
+                              
+                                  sh """
+                                    echo "Running Helm"
+                                   helm install prod${BUILD_NUMBER} ./HELM/onboard-task --values ./HELM/onboard-task/prod.yaml
+                                    """
+                               }
 }
