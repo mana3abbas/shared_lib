859e347ed913812093ff3267f5cc16e4fa98a3c0
mana3abbas <112072190+mana3abbas@users.noreply.github.com>
2023-06-07 11:15:10 +0300

    Update frontend.groovy

--- vars/frontend.groovy
+++ vars/frontend.groovy
@@ -1,4 +1,4 @@
-def call (name){
+def call(name){
                      sh """
                           docker login -u $USERNAME -p $PASSWORD
                             docker build -t monasamir/${name}:c${BUILD_NUMBER} $WORKSPACE/badreads-frontend/
